8897eb28a81e0fa91145cd9a1e9884b8b0c672b9
Jack <jackgan@JackdeMacBook-Pro.local>
2018-10-12 22:10:26 +1300

    12

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var config = {
   encryptBytes: 128
 };
 
-app.use (express.static(path.join(__dirname + '/front-end')))
+app.use (express.static(path.join(__dirname + '/front-end/js')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 //invoke functions on a service hosted in a different location
