53ffb1f0759f7ef3b0cc42b0a76e8e2fc0cec38c
Jack <jackgan@JackdeMacBook-Pro.local>
2018-10-13 19:58:21 +1300

    fix url

--- database/db.sql
+++ database/db.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (
   username varchar(32) NOT NULL,
   email varchar(32) NOT NULL,
-  encrypted_password varchar(128) NOT NULL,
-  salt varchar(64) NOT NULL,
+  encrypted_password varchar(256) NOT NULL,
+  salt varchar(128) NOT NULL,
   PRIMARY KEY (email)
-) DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+heroku addons:create heroku-postgresql WITH ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8'
