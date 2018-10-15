bbc859525e1402c8573c670040172301af44c702
Jack <jackgan@JackdeMacBook-Pro.local>
2018-10-15 21:56:10 +1300

    added cart

--- database/db.sql
+++ database/db.sql
@@ -16,7 +16,18 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE  cart (
+  email varchar(32) NOT NULL,
+  productId INTEGER NOT NULL,
+  productName varchar(32) NOT NULL,
+  amount INTEGER NOT NULL,
+  totalPrice MONEY NOT NULL,
+  FOREIGN KEY (email) REFERENCES users (email),
+  FOREIGN KEY (productId, productName) REFERENCES products (id,name),
+  PRIMARY KEY (productId,email)
+);
 
 INSERT INTO products (name, price, description) VALUES('apple', 5, 'this is fruit');
-
 INSERT INTO products (name, price, description) VALUES('pear', 3, 'this is fruit as well');
+INSERT INTO cart (email, productId, productName, amount, totalPrice) VALUES('test@gmail.com',1, apple, 5, 10);
+INSERT INTO cart (email, productId, productName ,amount, totalPrice) VALUES('test@gmail.con',2, pear, 5, 10);
