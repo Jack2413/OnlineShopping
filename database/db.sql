976ddf142fd4218346bbc84c720e1e2897ceb4a7
Jack <jackgan@JackdeMacBook-Pro.local>
2018-10-16 19:14:41 +1300

    ok

--- database/db.sql
+++ database/db.sql
@@ -13,21 +13,42 @@ CREATE TABLE products (
   name varchar(32) NOT NULL,
   price money NOT NULL,
   description varchar(255) NOT NULL,
+  imgcode INTEGER NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE  cart (
   email varchar(32) NOT NULL,
   productId INTEGER NOT NULL,
-  productName varchar(32) NOT NULL,
   amount INTEGER NOT NULL,
   totalPrice MONEY NOT NULL,
   FOREIGN KEY (email) REFERENCES users (email),
-  FOREIGN KEY (productId, productName) REFERENCES products (id,name),
+  FOREIGN KEY (productId) REFERENCES products (id),
   PRIMARY KEY (productId,email)
 );
 
-INSERT INTO products (name, price, description) VALUES('apple', 5, 'this is fruit');
-INSERT INTO products (name, price, description) VALUES('pear', 3, 'this is fruit as well');
-INSERT INTO cart (email, productId, productName, amount, totalPrice) VALUES('test@gmail.com',1, apple, 5, 10);
-INSERT INTO cart (email, productId, productName ,amount, totalPrice) VALUES('test@gmail.con',2, pear, 5, 10);
+CREATE TABLE orders (
+  email varchar(32) NOT NULL,
+  orderID serial NOT NULL,
+  totalPrice MONEY NOT NULL,
+  theDate timestamp NOT NULL,
+  FOREIGN KEY (email) REFERENCES users (email),
+  PRIMARY KEY (orderID)
+);
+
+CREATE TABLE  orderDetails (
+  orderID INTEGER NOT NULL,
+  email varchar(32) NOT NULL,
+  productId INTEGER NOT NULL,
+  amount INTEGER NOT NULL,
+  totalPrice MONEY NOT NULL,
+  FOREIGN KEY (email) REFERENCES users (email),
+  FOREIGN KEY (productId) REFERENCES products (id),
+  FOREIGN KEY (orderID) REFERENCES order (orderID),
+  PRIMARY KEY (orderID,productId,email)
+);
+
+INSERT INTO products (name, price, description) VALUES('apple', 5, 'this is fruit',1);
+INSERT INTO products (name, price, description) VALUES('pear', 3, 'this is fruit as well',2);
+INSERT INTO cart (email, productId, amount, totalPrice) VALUES('test@gmail.com',1 , 5, 10);
+INSERT INTO cart (email, productId, amount, totalPrice) VALUES('test@gmail.com',2,, 5, 10);
