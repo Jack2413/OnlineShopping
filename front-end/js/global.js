e66dfac04a45f67b765a18dea1679f4b23b89438
Jack <jackgan@JackdeMacBook-Pro.local>
2018-11-02 15:30:06 +1300

    added

--- front-end/js/global.js
+++ front-end/js/global.js
@@ -178,15 +178,16 @@ $(document).ready(function(e) {
 		function(result){
 			alert(result.feedback);
 			if(result.status==200){
+				window.location.href = window.location.href = url+"/index.html";
 				var HTML =  '<ul class="navbar-nav ml-auto">';
 					HTML += '<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>';
 	          		HTML += '<li class="nav-item"><a class="nav-link" href="cart.html">Cart</a></li>';
 	          		HTML += '<li class="nav-item"><a class="nav-link" href="#">username</a></li>';
 	          		HTML += '<li class="nav-item" id = logout>logout</li></ul>';
 
-          		$('#navbarResponsive').remove();
+          		$('#navbarResponsive').empty();
 				$('#navbarResponsive').prepend(HTML);
-				window.location.href = window.location.href = url+"/index.html";;
+				
 			}
 			
 		}, 
@@ -203,7 +204,7 @@ $(document).ready(function(e) {
 	    HTML += '<li class="nav-item"><a class="nav-link" href="#">username</a></li>';
 	    HTML += '<li class="nav-item" id = logout>logout</li></ul>';
 	    alert($('#navbarResponsive').val());
-    $('#navbarResponsive').remove();
+    $('#navbarResponsive').empty();
 	$('#navbarResponsive').prepend(HTML);
 
 	$('#RegisterButton').click(function(){
