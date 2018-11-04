6e2745d0fbff5e77eca96eef8f8da3ef27bf1bff
Jack <jackgan@JackdeMacBook-Pro.local>
2018-11-04 18:16:17 +1300

    update order

--- front-end/js/order.js
+++ front-end/js/order.js
@@ -2,6 +2,32 @@ var url = 'https://nwen304onlineshoping.herokuapp.com';
 var ERROR_LOG = console.error.bind(console);
 $(document).ready(function(e) {
 	loadOrder();
+	$('#viewButton').click(function() {
+		var $orderInfo $(this).parent('tr');
+		var $orderid = $orderInfo.find('.orderid').text();
+		alert(orderid);
+
+		$.ajax({
+				method: 'POST',
+				url: url+'/order',
+				data: JSON.stringify({
+					orderid: $orderid
+				}),
+				contentType: "application/json",
+				datatype: "json"
+
+		}).then (
+		function(result){
+			if(result.status==200){
+				
+			}	
+		}, 
+		function(error){
+			alert(error);
+
+		});
+		//alert($username+' '+$email+' '+$password);
+	});
 });
 
 function loadOrder () {
@@ -24,12 +50,15 @@ function loadOrderData(orders){
 		//alert('ID: $1 Task: $2 Name: $3',task.id,task.task,task.name);
 		
 		var orderHTML = '<tr><th scope="row">'+count+'</th>'
-        orderHTML += '<td>'+order.email+'</td>'
-        orderHTML += '<td>'+order.orderid+'</td>'
-        orderHTML += '<td>'+order.thedate+'</td>'
-        orderHTML += '<td><a href="viewOrder.html" class="btn btn-success">View</a></td></tr>'
+        orderHTML += '<td class="email">'+order.email+'</td>'
+        orderHTML += '<td class="orderid">'+order.orderid+'</td>'
+        orderHTML += '<td class="thedate">'+order.thedate+'</td>'
+        orderHTML += '<td><a href="viewOrder.html" class="btn btn-success" id="viewButton">View</a></td></tr>'
 			
 		$('#orderbody').prepend(orderHTML);
 		count ++;
 	});
 }
+
+
+
