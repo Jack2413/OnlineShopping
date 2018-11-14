b7eb4540f5638cb05a6b8aec88b2fe5f68eb7899
Jack <jackgan@JackdeMacBook-Pro.local>
2018-11-14 23:09:48 +1300

    #4 test email

--- index.js
+++ index.js
@@ -400,8 +400,9 @@ function sendAnResetEmail(email,token){
 	  from: 'nwen304onlingshoping@gmail.com',
 	  to: '888jack219@gmail.com',
 	  subject: 'Sending Email using Node.js',
-	  text: 'click the link below to reset the password'+'\n'+
-	  +'https://nwen304onlineshoping.herokuapp.com/ForgotReset.html/'+token
+	  text: 'click the link below to reset the password'+
+	  'https://nwen304onlineshoping.herokuapp.com/ForgotReset.html/'+
+    token
 	};
 
 	transporter.sendMail(mailOptions, function(error, info){
