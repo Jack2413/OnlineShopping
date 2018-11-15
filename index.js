71652631fd592c20d8bdf6fe81912e9a735c2f9d
Jack <jackgan@JackdeMacBook-Pro.local>
2018-11-15 16:29:24 +1300

    #4 add app get by token

--- index.js
+++ index.js
@@ -388,8 +388,8 @@ app.get("/forgot/:email", async (req, res) => {
 });
 
 app.get('/ForgotReset/:token', function(req, res) {
-  res.sendfile(__dirname + '/front-end/ForgotReset.html');
-  //res.render('pages/ForgotReset');
+  //res.sendfile(__dirname + '/front-end/ForgotReset.html');
+  res.render('pages/ForgotReset');
 });
 
 app.put("/ForgotReset/:token", async (req, res) => {
